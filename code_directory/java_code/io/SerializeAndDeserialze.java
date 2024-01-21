9768bbfcaa8412ba37df6857ccc2c28c65f6f891
0310AJ2422 <munirajrb5328@gamil.com>
2024-01-21 03:09:24 -0800

    update : update exist program

--- code_directory/java_code/io/SerializeAndDeserialze.java
+++ code_directory/java_code/io/SerializeAndDeserialze.java
@@ -3,11 +3,41 @@ package code_directory.java_code.io;
 
 import java.io.*;
 public class SerializeAndDeserialze {
-    public static void main(String test[]){
-        try{
-        testableClass tc=new testableClass(1, "tamizhi");
+    public static void main(String test[]) throws ClassNotFoundException{
+        
+        //serialize an object 
+        try(ObjectOutputStream obs=new ObjectOutputStream(new FileOutputStream("files.bat"))){
+            testableClass test1=new testableClass(1, "tamizhi");
+            obs.writeObject(test1);
         }catch(IOException e){
+            e.printStackTrace();    
+        }
+
+        //deserialize an object 
+        try(ObjectInputStream ois=new ObjectInputStream(new FileInputStream("files.bat"))){
+                
+            Object obj=ois.readObject();
             
+            if(obj instanceof testableClass){
+                testableClass test2=(testableClass)obj;
+
+                //use objectstreamclass.lookup to get information of class
+                ObjectStreamClass osc= ObjectStreamClass.lookup(testableClass.class);
+
+                System.out.println("Class name: "+osc.getName());
+                System.out.println("serialversionUID "+osc.getSerialVersionUID());
+
+                //get information about the field of the class 
+
+                ObjectStreamField[] osf=osc.getFields();
+                for(ObjectStreamField fs:osf){
+                    System.out.println("Field name "+fs.getName());
+                    System.out.println("Field type "+fs.getType());
+                }
+                System.out.println("deserialize object "+test2);
+            }
+        }catch(IOException e){
+            e.printStackTrace();
         }
     }
 }
@@ -17,10 +47,7 @@ class testableClass implements Serializable{
     private int id ;
     private String name;
     
-    testableClass(int id,String name){
-        id=this.id;
-        name=this.name;
-    }
+    
 
     public int getId() {
         return id;
@@ -38,6 +65,14 @@ class testableClass implements Serializable{
         this.name = name;
     }
 
+    testableClass(int id,String name){
+        id=this.id;
+        name=this.name;
+    }
     
+     @Override
+    public String toString() {
+        return "MyClass{field1=" + id + ", field2='" + name + '\'' + '}';
+    }
     
 }
