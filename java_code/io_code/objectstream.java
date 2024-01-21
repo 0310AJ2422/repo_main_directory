a0388e2fcfbd244fbd84a401911a3ea0436f9ac6
0310AJ2422 <munirajrb5328@gamil.com>
2024-01-20 20:47:53 -0800

    update code : alter entier code

--- java_code/io_code/objectstream.java
+++ java_code/io_code/objectstream.java
@@ -1,37 +1,33 @@
 package java_code.io_code;
 
-import java.io.ObjectStreamClass;
-import java.io.ObjectStreamField;
-import java.lang.reflect.Field;
+import java.io.*;
 
-public class objectstream{
-    public static void main(String args[]){
-        ObjectStreamClass osc=ObjectStreamClass.lookup(MyClass.class);
-        Class<?> oscForClass = osc.forClass();
 
-        System.out.println("Original ObjectStreamClass"+ osc);
-        System.out.println("class name "+osc.getName());
-        System.out.println("SerialVersionUID "+osc.getSerialVersionUID());
-        System.out.println("fileds");
+ public class objectstream {
+    public static void main(String[] args) {
+        ObjectStreamClass osc = ObjectStreamClass.lookup(MyClass.class);
+        Class<?> osc1=osc.forClass();
 
-        for(ObjectStreamField field : osc.getFields()){
-            System.out.println("  "+field.getName()+" : "+field.getType());
-        }
-
-        System.out.println("ObjectStreamClass obtained with forClass()"+ oscForClass);
-        System.out.println("class name "+oscForClass.getName());
-       // System.out.println("SerialVersionUID "+oscForClass.getSerialVersionUID());
-        System.out.println("fileds");
-        for(Field fields : oscForClass.getFields()){
-            System.out.println("  "+fields.getName()+" : "+fields.getType());
-        }
-
+        System.out.println(osc1.getName());
+        System.out.println(osc.getSerialVersionUID());
     }
 }
-class MyClass implements java.io.Serializable {
-    private static final long serialVersionUID = 1L;
-    private int id;
-    private String name;
 
-    // Other class members and methods
+class MyClass implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private int myInt;
+    private String myString;
+
+    public MyClass(int myInt, String myString) {
+        this.myInt = myInt;
+        this.myString = myString;
+    }
+
+    public int getMyInt() {
+        return myInt;
+    }
+
+    public String getMyString() {
+        return myString;
+    }
 }
